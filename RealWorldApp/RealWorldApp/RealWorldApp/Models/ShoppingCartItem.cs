7e693b864bf74f7445b527879dea38111b77bcaa
Selmancan Kılınç <selmancan0638@gmail.com>
2023-04-29 14:13:10 +0300

    Initial commit

---
+++ RealWorldApp/RealWorldApp/RealWorldApp/Models/ShoppingCartItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWorldApp.Models
+{
+    public class ShoppingCartItem
+    {
+        //public int id { get; set; }
+        //public double price { get; set; }
+        //public decimal totalAmount { get; set; }
+        //public int qty { get; set; }
+        public string ProductName { get; set; }
+
+
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public int Qty { get; set; }
+        public double TotalAmount { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
